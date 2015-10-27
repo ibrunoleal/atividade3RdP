06fd178d94f44a9276d0ae1f4b97f9e915c63004
ibrunoleal <ibrunoleal@gmail.com>
2015-10-27 10:25:49 -0300

    resolvido 1.5

--- RdPAtividade3/src/br/ufc/arida/bcl/rp20152/exercicios/Exercicio1.java
+++ RdPAtividade3/src/br/ufc/arida/bcl/rp20152/exercicios/Exercicio1.java
@@ -1,7 +1,10 @@
 package br.ufc.arida.bcl.rp20152.exercicios;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 import br.ufc.arida.bcl.rp20152.entidades.Matriz;
 
@@ -13,8 +16,11 @@ public class Exercicio1 {
 		 * ************ Dados de entrada ***************************************
 		 */
 		double[] dadosU = { 3, 1, 2 };
-		ArrayRealVector u = new ArrayRealVector(dadosU);
-
+		RealVector u = new ArrayRealVector(dadosU);
+		
+//		System.out.println("Vetor ua: " + ua);
+//		System.out.println("Vetor ub: " + ub);
+		
 		double[][] dadosK = { { 0.5, 0.2, 0.1 }, { 0.6, 0.18, 0.4 }, { 0.1, 0.6, 0.2 } };
 		Matriz K = new Matriz(dadosK);
 
@@ -123,10 +129,37 @@ public class Exercicio1 {
 		System.out.println("\nMatriz EIa_b:\n" + EIa_b);
 		
 		Matriz EIIa_b = funcaoSigmaDeADadoB(EII);
-		System.out.println("\nMatriz EIa_b:\n" + EIIa_b);
+		System.out.println("\nMatriz EIIa_b:\n" + EIIa_b);
 		
 		Matriz EIIIa_b = funcaoSigmaDeADadoB(EIII);
-		System.out.println("\nMatriz EIa_b:\n" + EIIIa_b);
+		System.out.println("\nMatriz EIIIa_b:\n" + EIIIa_b);
+		
+		/*
+		 * ************ 1.5) ***************************************
+		 */
+		System.out.println("1.5)---------------------------------------\n");
+		RealVector ua = u.getSubVector(0, 2);
+		RealVector ub = u.getSubVector(2, 1);
+		System.out.println("Vetor u: " + u);
+		System.out.println("Vetor ua: " + ua);
+		System.out.println("Vetor ub: " + ub);
+		
+		double[] xaData = {0,0.5};
+		RealVector xa = new ArrayRealVector(xaData);
+		double[] xbData = {1};
+		RealVector xb = new ArrayRealVector(xbData);
+		System.out.println("Vetor xa: " + xa);
+		System.out.println("Vetor xb: " + xb);
+		
+		RealVector EIu_a_b = funcaoMiADadoB(EI, ua, ub, xb);
+		System.out.println("\nVetor Ua_b para EI:\n" + EIu_a_b);
+		
+		RealVector EIIu_a_b = funcaoMiADadoB(EII, ua, ub, xb);
+		System.out.println("\nVetor Ua_b para EII:\n" + EIIu_a_b);
+		
+		RealVector EIIIu_a_b = funcaoMiADadoB(EIII, ua, ub, xb);
+		System.out.println("\nVetor Ua_b para EIII:\n" + EIIIu_a_b);
+
 	}
 
 	
@@ -161,6 +194,17 @@ public class Exercicio1 {
 		Matriz resultado = new Matriz(xab.multiply(xbb));
 		resultado = new Matriz(resultado.multiply(xba));
 		resultado = new Matriz(xaa.subtract(resultado));
+		return resultado;
+	}
+	
+	public static RealVector funcaoMiADadoB(Matriz matriz, RealVector ua, RealVector ub, RealVector xb) {
+		Matriz mab = getParticaoAB(matriz);
+		Matriz mbb = new Matriz(MatrixUtils.inverse(getParticaoBB(matriz)));
+		Matriz maux = new Matriz(mab.multiply(mbb));
+		RealVector vetoraux = new ArrayRealVector(xb.subtract(ub));
+		
+		RealVector resultado = maux.operate(vetoraux);
+		resultado = ua.subtract(resultado);
 		return resultado;
 	}
 
